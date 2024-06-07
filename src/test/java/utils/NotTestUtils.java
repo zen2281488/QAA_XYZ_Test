5c8791e934039ca17239a0225785bc9f6df217a4
zen2281488 <today_na@mail.ru>
2024-06-07 06:20:52 +0300

    refactoring Codestyle, readme.md

--- src/test/java/utils/NotTestUtils.java
+++ src/test/java/utils/NotTestUtils.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class NotTestUtils {
     public static int fibonacci() {
-        //Для проверки использовал следующий ряд 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, … , ∞
+        //Для проверки использовал следующий ряд: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, … , ∞
         int n = LocalDate.now().getDayOfMonth() + 1;
         int a = 0;
         int b = 1;
